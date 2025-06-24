3ddb921572393b9d99b760fd6451857b410ba715
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-06-24 16:04:22 +0200

    initial commit

---
+++ backend/src/main/java/com/mindstore/backend/BackendApplication.java
@@ -0,0 +1,23 @@
+package com.mindstore.backend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.out.println("Read: " +  dotenv.get("SPRING_DATASOURCE_USERNAME") +  " and: " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		SpringApplication.run(BackendApplication.class, args);
+
+
+	}
+
+}
---
+++ frontend/README.md
@@ -0,0 +1,20 @@
+# Mindstore
+
+Search engine application that allows searching for IT terms and topics and adding new files to the data store.
+
+## Used technologies
+
+- Angular, TypeScript
+- Docker
+- Java
+- Spring Boot, Spring Security
+- Postgres
+- Opensearch
+
+## Deployment
+
+1. Run the following command to set up the database:
+
+```
+docker compose up --build -d postgres
+```

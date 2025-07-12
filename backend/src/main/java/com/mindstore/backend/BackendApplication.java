67f9563909fb81d06da265fe0e63a8fa065ce862
schikimikiviki <viktoria.bzdyk@gmx.at>
2025-07-12 16:57:34 +0200

    integrated flyway

--- backend/src/main/java/com/mindstore/backend/BackendApplication.java
+++ backend/src/main/java/com/mindstore/backend/BackendApplication.java
@@ -18,7 +18,7 @@ public class BackendApplication {
 		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
 		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.out.println("Read: " +  dotenv.get("SPRING_DATASOURCE_USERNAME") +  " and: " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		//System.out.println("Read: " +  dotenv.get("SPRING_DATASOURCE_USERNAME") +  " and: " + dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
 
 		SpringApplication.run(BackendApplication.class, args);
